7bc812c021e17adf4a67a90446a68a2ea6a5bb03
MariaFernanda <mafaduartemelot216@gmail.com>
2025-10-13 17:55:48 -0300

    refactor:ajustes na classe Produto (atributos e constructor)

--- src/main/java/com/estoque_mercado/estoque/model/Produto.java
+++ src/main/java/com/estoque_mercado/estoque/model/Produto.java
@@ -1,30 +1,52 @@
 package com.estoque_mercado.estoque.model;
-
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Data
 public class Produto {
+    @Getter
+    @Setter
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-    private int quantity;
-    private double price;
+    private String nome;
+    private String descricao;
+    private BigDecimal precoVenda;
+    private Integer quantidadeEstoque;
+    private LocalDate dataValidade;
 
-    public Long getId() { return id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    //Muitos produtos pertencem a uma categoria
+    @ManyToOne
+    @JoinColumn(name = "categoria_id") // nome da coluna no banco
+    private Categoria categoria;
 
-    public Integer getQuantity() { return quantity; }
-    public void setQuantity(Integer quantity) { this.quantity = quantity; }
+    //Muitos produtos pertencem a um fornecedor
+    @ManyToOne
+    @JoinColumn(name = "fornecedor_id") // chave estrangeira no banco
+    private Fornecedor fornecedor;
 
-    public Double getPrice() { return price; }
-    public void setPrice(Double price) { this.price = price; }
-
+    public Produto(Categoria categoria, LocalDate dataValidade, String descricao, Fornecedor fornecedor, Long id, String nome, BigDecimal precoVenda, Integer quantidadeEstoque) {
+        this.categoria = categoria;
+        this.dataValidade = dataValidade;
+        this.descricao = descricao;
+        this.fornecedor = fornecedor;
+        this.id = id;
+        this.nome = nome;
+        this.precoVenda = precoVenda;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+    
 }
