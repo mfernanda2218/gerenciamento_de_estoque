5ebef80e16016188f15f90db05b0780c164cdcb0
MariaFernanda <mafaduartemelot216@gmail.com>
2025-10-27 14:21:11 -0300

    refactor: ajuste

--- src/main/java/com/estoque_mercado/estoque/repository/EntradaEstoqueRepository.java
+++ src/main/java/com/estoque_mercado/estoque/repository/EntradaEstoqueRepository.java
@@ -1,11 +1,15 @@
 package com.estoque_mercado.estoque.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.estoque_mercado.estoque.model.EntradaEstoque;
+import com.estoque_mercado.estoque.model.Fornecedor;
 
 @Repository
 public interface EntradaEstoqueRepository extends JpaRepository<EntradaEstoque, Long> {
-
+    EntradaEstoque findByCodigo(String codigo);
+    List <EntradaEstoque> findByFornecedor(Fornecedor fornecedor);
 }
